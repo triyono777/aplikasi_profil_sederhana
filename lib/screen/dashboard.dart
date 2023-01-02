0f61a2d73c9f3dd4445f359e4d5e99fa5afc27ff
Triyono <yono.tri@gmail.com>
2023-01-02 08:44:42 +0700

    add app bar

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -10,6 +10,10 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List kontak'),
+      ),
+    );
   }
 }
