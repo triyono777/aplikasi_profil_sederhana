c18bed382917a6521735e3ea911bf3bdc398655b
Triyono <yono.tri@gmail.com>
2023-01-02 09:42:24 +0700

    pindah halaman

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:aplikasi_pertama/screen/profil.dart';
 import 'package:flutter/material.dart';
 import 'package:aplikasi_pertama/data/data_profil.dart';
 
@@ -21,7 +22,14 @@ class _DashboardState extends State<Dashboard> {
         itemCount: dataProfil.listProfil.length,
         itemBuilder: (_, index) => Card(
           child: InkWell(
-            onTap: () {},
+            onTap: () {
+              print('di pencet ${dataProfil.listProfil[index].nama}');
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => Profil(),
+                ),
+              );
+            },
             child: ListTile(
               title: Text('${dataProfil.listProfil[index].nama}'),
               subtitle: Text('${dataProfil.listProfil[index].alamat}'),
