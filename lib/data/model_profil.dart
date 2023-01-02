1184e313f1caf5be7f08cdf17e6007f2886e5225
Triyono <yono.tri@gmail.com>
2023-01-02 09:03:25 +0700

    add model

---
+++ lib/data/model_profil.dart
@@ -0,0 +1,13 @@
+class ModelProfil {
+  String? nama;
+  String? noHape;
+  String? pekerjaan;
+  String? alamat;
+
+  ModelProfil({
+    this.nama,
+    this.alamat,
+    this.noHape,
+    this.pekerjaan,
+  });
+}
