a395642d2faa2c03a37a227b38eb37c077215e67
Triyono <yono.tri@gmail.com>
2023-01-02 08:27:19 +0700

    change color

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: Profil(),
     );
