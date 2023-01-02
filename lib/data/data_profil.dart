d3ffc0e4e4ec3eadef9b18a4a2a2ec5811c93cd2
Triyono <yono.tri@gmail.com>
2023-01-02 09:12:59 +0700

    add  data profil

---
+++ lib/data/data_profil.dart
@@ -0,0 +1,30 @@
+import 'package:aplikasi_pertama/data/model_profil.dart';
+
+class DataProfil {
+  List<ModelProfil> listProfil = [
+    ModelProfil(
+      nama: 'budi',
+      alamat: 'sragen',
+      noHape: '09988888',
+      pekerjaan: 'desain ui ux',
+    ),
+    ModelProfil(
+      nama: 'alex',
+      alamat: 'solo',
+      noHape: '09988888',
+      pekerjaan: 'desain ui ux',
+    ),
+    ModelProfil(
+      nama: 'rudi',
+      alamat: 'sragen',
+      noHape: '09988888',
+      pekerjaan: 'desain ui ux',
+    ),
+    ModelProfil(
+      nama: 'andi',
+      alamat: 'sragen',
+      noHape: '09988888',
+      pekerjaan: 'desain ui ux',
+    ),
+  ];
+}
